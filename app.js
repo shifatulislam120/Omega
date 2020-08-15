f25698b4f864ffa19648f4d271f2551426353073
shifatulislam120 <69710122+shifatulislam120@users.noreply.github.com>
2020-08-15 01:22:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCCP0ba9uQnj8ew61BUx8BWspshFe9dAW0",
+  authDomain: "faceb0ok-f745c.firebaseapp.com",
+  databaseURL: "https://faceb0ok-f745c.firebaseio.com",
+  projectId: "faceb0ok-f745c",
+  storageBucket: "faceb0ok-f745c.appspot.com",
+  messagingSenderId: "683450335765",
+  appId: "1:683450335765:web:158fd49e8d1219b588c41c",
+  measurementId: "G-HXXEGXK3F9"
 };
-
      
  
 ////Do Not Edit Below This
